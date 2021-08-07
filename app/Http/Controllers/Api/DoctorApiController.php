eafcc5f289b5b02db7ba1184f8f9f859d4187382
John Mwakalinga <mwakalingajohn@gmail.com>
2021-08-07 16:33:01 +0300

    - resourcify

--- app/Http/Controllers/Api/DoctorApiController.php
+++ app/Http/Controllers/Api/DoctorApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DoctorResource;
 use App\Models\Doctor;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,6 @@ class DoctorApiController extends Controller
 {
     public function index()
     {
-        return Doctor::all();
+        return DoctorResource::collection(Doctor::all());
     }
 }
